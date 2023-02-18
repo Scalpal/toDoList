8ff5f4dd20abb91fa4b7ce3edadfdd00f7b0d82d
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-02-18 23:16:41 +0100

    Fix: spelling error on import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css"
 import { useState, createContext } from "react"
 import { useRouter } from "next/router"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Task from "../components/Task"
 
 const initialList = [
